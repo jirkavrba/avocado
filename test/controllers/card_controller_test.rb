01393f98a5c7a08f7caf49634717e274c8fb83ab
Jiří Vrba <jiri@vrba.dev>
2020-11-28 01:43:34 +0100

    Added another 2 testcases for card controller

--- test/controllers/card_controller_test.rb
+++ test/controllers/card_controller_test.rb
@@ -9,7 +9,7 @@ class CardControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'users can manage their collection cards' do
-    sign_in_as(:normal_user)
+    sign_in_as :normal_user
 
     card_collection = card_collections(:public_owned_by_normal_user)
 
@@ -19,14 +19,14 @@ class CardControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'users cannot edit other users collection cards' do
-    sign_in_as(:normal_user)
+    sign_in_as :normal_user
 
     get card_collection_cards_path(card_collections(:public_owned_by_admin_user))
     assert_response :forbidden
   end
 
   test 'admins can manage other users collection cards' do
-    sign_in_as(:admin_user)
+    sign_in_as :admin_user
 
     card_collection = card_collections(:public_owned_by_normal_user)
 
@@ -34,4 +34,36 @@ class CardControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'h2', "Manage cards of #{card_collection.title}"
   end
+
+  test 'users cannot add cards to other users collections' do
+    sign_in_as :normal_user
+
+    post card_collection_cards_path(card_collections(:public_owned_by_admin_user)),
+         params: {
+           card: {
+             title: 'Some title',
+             question: 'This is not actually that much important tbh'
+           }
+         }
+
+    assert_response :forbidden
+  end
+
+  test 'admins can add cards to other users collections' do
+    sign_in_as :admin_user
+
+    card_collection = card_collections(:public_owned_by_normal_user)
+
+    post card_collection_cards_path(card_collection),
+         params: {
+           card: {
+             title: 'Some title',
+             question: 'This is not actually that much important tbh'
+           },
+           return: true
+         }
+
+    assert_redirected_to card_collection_path(card_collection)
+  end
 end
+
